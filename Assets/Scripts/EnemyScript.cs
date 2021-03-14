6e990eb2843a1aa01c0701d0bbfa539d4c694529
Guhanesh <guhanesh@live.com>
2021-03-15 00:35:11 +0530

    Enemy Script Updated

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class EnemyScript : MonoBehaviour
 {
    [SerializeField] private GameObject bullet;
+   [SerializeField] private LayerMask playerLayer;
+   private float recoilTimer=-1;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +16,56 @@ public class EnemyScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if  (recoilTimer==0)
+        {
+RaycastHit hit;
+      
+        if (Physics.Raycast(transform.position, transform.forward, out hit,10f,playerLayer))
+        {
+              var bullet =ObjectPoolingScript.SharedInstance.GetPooledObject();
+        print( bullet.transform.position);
+        bullet.Fire( transform.position, transform.eulerAngles, gameObject.layer,transform.forward);
+        bullet.gameObject.SetActive(true);
+        recoilTimer=-1;
+          
+        }
         
+                if (Physics.Raycast(transform.position, -transform.forward, out hit,10f,playerLayer))
+        {
+               var bullet =ObjectPoolingScript.SharedInstance.GetPooledObject();
+        print( bullet.transform.position);
+        bullet.Fire( transform.position, transform.eulerAngles, gameObject.layer,-transform.forward);
+        bullet.gameObject.SetActive(true);
+          recoilTimer=-1;
+        }
+
+         if (Physics.Raycast(transform.position, transform.right, out hit,10f,playerLayer))
+        {
+            var bullet =ObjectPoolingScript.SharedInstance.GetPooledObject();
+        print( bullet.transform.position);
+        bullet.Fire( transform.position, transform.eulerAngles, gameObject.layer, transform.right);
+        bullet.gameObject.SetActive(true);
+          recoilTimer=-1;
+        }
+
+         if (Physics.Raycast(transform.position, -transform.right, out hit,10f,playerLayer))
+        {
+        var bullet =ObjectPoolingScript.SharedInstance.GetPooledObject();
+        print( bullet.transform.position);
+        bullet.Fire( transform.position, transform.eulerAngles, gameObject.layer, -transform.right);
+        bullet.gameObject.SetActive(true);
+         recoilTimer=-1;
+        }
+        
+        }
+          
+        if  (recoilTimer<0)
+        {
+        recoilTimer+=Time.deltaTime;
+        }
+        else
+        {
+            recoilTimer=0;
+        }
     }
 }
